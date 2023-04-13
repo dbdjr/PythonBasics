7bff32d3916329a6f45117526e99ccc016bb3c93
dbdjr <dbdjr123@gmail.com>
2023-04-13 16:01:15 -0300

    Cap3 finished

--- Cap. 03/exemploDeltas_start.py
+++ Cap. 03/exemploDeltas_start.py
@@ -7,3 +7,20 @@ from datetime import time
 from datetime import datetime
 from datetime import timedelta
 
+def quantosFaltam(ano, mes, dia):
+    hoje  = date.today()
+    dataProcurada = date(ano,mes,dia)
+    if dataProcurada > hoje:
+        data = dataProcurada - hoje
+        mensagem = "Faltam "+ str(data).replace("days, 0:00:00","")+"dias"
+        return mensagem
+    elif dataProcurada < hoje:
+        d = dataProcurada - hoje
+        data = str(d).replace("days, 0:00:00", "")
+        mensagem = "Passaram " + data.replace("-","")+"dias"
+        return mensagem
+    else:
+        mensagem = "A data é hoje!"
+        return mensagem
+    
+print(quantosFaltam(2022,4,13))
--- Cap. 03/exemplocalendarios_start.py
+++ Cap. 03/exemplocalendarios_start.py
@@ -1,3 +1,12 @@
 #
 # Arquivo com exemplos de uso de calendários
 #
+import calendar
+
+def imprimeMes():
+    for mes in calendar.month_name:
+        print(mes)
+    for dias in calendar.day_name:
+        print(dias)
+
+imprimeMes()
