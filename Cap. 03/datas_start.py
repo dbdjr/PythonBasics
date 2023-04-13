a6ba2deb26dd943663a19fcbade3518e78fecfeb
Wanessa Mendonca <dbdjr123@gmail.com>
2023-04-13 01:16:36 -0300

    Manipulando Import DateTime

--- Cap. 03/datas_start.py
+++ Cap. 03/datas_start.py
@@ -1,4 +1,20 @@
 #
-# Arquivo com exemplos de manipulação de  datas
-#
+# Arquivo com exemplos de manipulação de  data
 
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+def dataHora():
+    hoje = date.today()
+    print("Hoje é: ", hoje)
+    print("Hoje é: ", hoje.day, hoje.month, hoje.year)
+    print("Hoje é: ", hoje.weekday())
+    dias = ["seg","ter","qua","qui","sex","sab","dom"]
+    print("Hoje é: ", dias[hoje.weekday()])
+
+    data= datetime.now()
+    print("Data e hora", data)
+    tempo = datetime.time(data)
+    print("Hora atual: ", tempo)
+dataHora()
