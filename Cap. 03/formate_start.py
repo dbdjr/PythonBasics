1072820e75d542d8bd3186083fc3fe650de8dd35
Wanessa Mendonca <dbdjr123@gmail.com>
2023-04-13 12:01:06 -0300

    Aula Format Date

--- Cap. 03/formate_start.py
+++ Cap. 03/formate_start.py
@@ -7,29 +7,29 @@ from datetime import datetime
 
 # Datas e horas podem ser formatados usando um conjunto de strings predefinidas
   
-# def formatHoje():
+def formatHoje():
 # #### Date Formatting ####
   
 # # %y/%Y - Ano, %a/%A - Dia da semana, %b/%B - Mês, %d - dia do mÊs
-#     hoje = datetime.now()
-#     print("Data e hora",hoje)
+    hoje = datetime.now()
+    print("Data e hora",hoje)
 
-#     print(hoje.strftime("O Ano de hoje é: %Y"))
-#     print(hoje.strftime("O Dia da semana de hoje é: %A"))
-#     print(hoje.strftime("O Mês de hoje é: %B"))
-#     print(hoje.strftime("O dia de hoje é: %d"))
+    print(hoje.strftime("O Ano de hoje é: %Y"))
+    print(hoje.strftime("O Dia da semana de hoje é: %A"))
+    print(hoje.strftime("O Mês de hoje é: %B"))
+    print(hoje.strftime("O dia de hoje é: %d"))
 
 # formatHoje()
     
 
 #% c - data e hora da localidade,% x - data da localidade,% X - hora da localidade
-# def dataLocal():
-#     hoje = datetime.now()
-#     print(hoje.strftime("O dia, data e hora local é: %c"))
-#     print(hoje.strftime("O data local é: %x"))
-#     print(hoje.strftime("A hora local é: %X"))
+def dataLocal():
+    hoje = datetime.now()
+    print(hoje.strftime("O dia, data e hora local é: %c"))
+    print(hoje.strftime("O data local é: %x"))
+    print(hoje.strftime("A hora local é: %X"))
 
-# dataLocal()
+dataLocal()
 #### Time Formatting ####
 # %I/%H - 12/24 hpras, %M - minuto, %S - segundo, %p -  AM/PM
 
