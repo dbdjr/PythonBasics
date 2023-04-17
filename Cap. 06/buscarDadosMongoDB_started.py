b4c7f36a63b9178274f00510733de7980bb1328f
Felipe Mendonça <dbdjr123@gmail.com>
2023-04-17 00:57:37 -0300

    Finalizado

--- Cap. 06/buscarDadosMongoDB_started.py
+++ Cap. 06/buscarDadosMongoDB_started.py
@@ -4,3 +4,20 @@
 import pymongo
 from pymongo import MongoClient
 
+
+def manipulaMDB ():
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client.conheca_python
+
+    for i in range(1,10):
+        obj = {'codigo': 1}
+        db.conheca_mongo.insert_one(obj)
+    
+    db.conheca_mongo.update_one({'codigo': 2},{'$set':{'NewAtribute':789}})
+    db.conheca_mongo.delete_one({'codigo': 1})
+
+    resconsult = db.conheca_mongo.find({})
+    for doc in resconsult:
+        print(doc)
+
+manipulaMDB()
--- Cap. 06/buscarDadosSQLite_start.py
+++ Cap. 06/buscarDadosSQLite_start.py
@@ -1,6 +1,34 @@
-# 
+#
 # Exemplo de acesso a uma base de dados SQLite
 #
 
 import sqlite3
 
+
+def manipulaDados(bancoDados, comando):
+    conexao = sqlite3.connect(bancoDados)
+    cursor = conexao.cursor()
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
+
+
+
+def selectData(bancoDados, comando):
+    conexao = sqlite3.connect(bancoDados)
+    cursor = conexao.cursor()
+    cursor.execute(comando)
+    linhas = cursor.fetchall()
+
+    for linha in linhas:
+        print(linha)
+
+def ManipulacaoDados():
+    comandoInsert = "INSERT INTO estados (nome_estado, sigla_estado, nome_capital) VALUES ('Estado Teste', 'EI', 'Teste')"
+    pathBD = "D:\\git\\arquivos_de_exercicios_descubra_o_python\\Cap. 06\\teste.db"
+    comandoSELECT = "SELECT nome_estado, sigla_estado, nome_capital FROM estados"
+    manipulaDados(pathBD, comandoInsert)
+    selectData(pathBD, comandoSELECT)
+
+
+ManipulacaoDados()
